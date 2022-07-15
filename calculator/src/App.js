58b02561e450901baf74a6f302eee34655d1daf2
Code-rina <katerina.kreibich@gmail.com>
2022-07-15 11:55:40 -0700

    App.js shell created

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+
+
+
 
 function App() { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h1 className="title">React Calculator</h1>
+        <div className="calculator">
+          <input type="text" className="calc-numbers" value="30"></input>
+          <div className="calculator-buttons">
+            <button className="btn-clear">C</button>
+            <button className="btn-orange">&larr;</button>
+            <button className="btn-orange">&divide;</button>
+            <button className="btn">7</button>
+            <button className="btn">8</button>
+            <button className="btn">9</button>
+            <button className="btn-orange">X</button>
+            <button className="btn">4</button>
+            <button className="btn">5</button>
+            <button className="btn">6</button>
+            <button className="btn">-</button>
+            <button className="btn">1</button>
+            <button className="btn">2</button>
+            <button className="btn">3</button>
+            <button className="btn-orange">+</button>
+            <button className="btn-zero">0</button>
+            <button className="btn-orange-equal">=</button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
